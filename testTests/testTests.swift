32ff180e07705403cf959600ffedc1b032a13b06
artem <artem.dan.com@gmail.com>
2025-01-27 03:50:49 +0300

    Первый раз

---
+++ testTests/testTests.swift
@@ -0,0 +1,17 @@
+//
+//  testTests.swift
+//  testTests
+//
+//  Created by Artem on 27.01.2025.
+//
+
+import Testing
+@testable import test
+
+struct testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
